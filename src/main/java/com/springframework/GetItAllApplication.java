5fab88735a6377b6f48c7040af5890282654eb5a
aayushkarki682 <ayush.karki12345@gmail.com>
2021-03-20 11:53:09 -0500

    first commit

---
+++ src/main/java/com/springframework/GetItAllApplication.java
@@ -0,0 +1,13 @@
+package com.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetItAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetItAllApplication.class, args);
+	}
+
+}
