05d759d8696ec4f1fc60b445eb39b35e85cccd9d
aayushkarki682 <ayush.karki12345@gmail.com>
2021-03-20 20:03:06 -0500

    signUp Completed

---
+++ src/main/java/com/springframework/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.springframework.controllers;
+
+import com.springframework.domain.User;
+import com.springframework.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getIndexPage(){
+        return "index";
+    }
+
+    @GetMapping("/signUp")
+    public String getSignUpPage(Model model){
+        model.addAttribute("user", new User());
+        return "signUp";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model){
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/signUpSuccess")
+    public String saveNewCustomer(@ModelAttribute User user){
+
+        userService.save(user);
+        return "redirect:/user/";
+    }
+
+
+}
